18bbe163fa8c99a16da12a0a1d3a2d6297450d90
peak90 <lidianshi0308@gmail.com>
2015-03-29 22:13:02 -0700

    change

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
-
+void ff();
 int main() {
 	cout << "merge with conflict resolution" << endl;
+	cout << "helo world";
 	cout << "add another two files" << endl;
 }
