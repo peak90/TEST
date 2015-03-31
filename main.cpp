8776ac9207172565f01ad70dc5c03a0e589da2c2
peak90 <lidianshi0308@gmail.com>
2015-03-30 22:02:25 -0700

    strange

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@ void fx();
 int main() {
 	cout << "xoxoxo merge againmerge with conflict resolution" << endl;
 	cout << "xxkdjalfadfhelo world xoxoxox";
+	cout << "i cannt do it"
 	cout << "add another xxfiles" << endl;
+	cout << "it is so wired"
 }
-
+fadfadfadfa
 void fx() {
 	int b = 10;
 	int c = a + b;
