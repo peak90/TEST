7f972e98bc184688d9c49038b29dfa3be41c159c
peak90 <lidianshi0308@gmail.com>
2015-04-01 11:00:02 -0700

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
 #include<iostream>
 using namespace std;
-xxx
-xx
-void fx();
-void fff()
+
 int main() {
-	cout << "xoxoxo merge againmerge with conflict resolution" << endl;
-	cout << "xxkdjalfadfhelo world xoxoxox";
-	cout << "i cannt do it"
-	cout << "add another xxfiles" << endl;
-	cout << "it is so wired"
-}
-fadfadfadfa
-void fx() {
-	int b = 10;
-	int c = a + b;
-	return c;
+	cout << "change the main.cpp in the branch";
+	cout << "learn git push and pull";
 }
