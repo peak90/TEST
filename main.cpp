4cdaf32b1c172b2d46ba57ad072753462b6fc5b5
peak90 <lidianshi0308@gmail.com>
2015-04-01 11:43:58 -0700

    xx

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main() {
 	cout << "change the main.cpp in the branch----";
 	cout << "learn git push and pull";
-	cout << "add some thing"
+	cout << "add somdfadfae thing"
+	cout << "adfafad";
 }
