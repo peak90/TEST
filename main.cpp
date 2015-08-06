345f08c9528fbb6b34f52a35b76b177166d70e16
peak90 <lidianshi0308@gmail.com>
2015-08-05 22:06:42 -0700

    add feature B v2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 void fff();
 int main() {
-	cout << "Add feature B, add feature A v3";
+	cout << "Add feature B, add feature A v3, add feature B v2";
 	cout << "better call soul";
 	cout << "come bobay";
 }
